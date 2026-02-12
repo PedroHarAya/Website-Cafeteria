7b933a1eb9006549aa30fd9068432b5ab2fc3f2a
PedroHarAya <p.hernandez21@info.uas.edu.mx>
2026-02-12 13:27:23 -0700

    Vista principal modificada con estilos

--- resources/views/Web.blade.php
+++ resources/views/Web.blade.php
@@ -64,6 +64,14 @@
             </nav>
         </div>
     </header>
+
+    <section class="banner">
+        <div class="content-banner">
+            <p>Café Delicioso</p>
+            <h2>100% Natural <br>Café Fresco</h2>
+            <a href="#">Comprar ahora</a>
+        </div>
+    </section>
 </body>
 
 </html>
