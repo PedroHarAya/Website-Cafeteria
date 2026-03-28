89d0cc27e6b0f6dd38f1bc72ce5db3835a4a47fe
PedroHarAya <p.hernandez21@info.uas.edu.mx>
2026-03-27 17:02:12 -0700

    agregamos los apartados de categorias y cartas de productos disponibles

--- resources/views/Web.blade.php
+++ resources/views/Web.blade.php
@@ -72,6 +72,101 @@
             <a href="#">Comprar ahora</a>
         </div>
     </section>
+
+    <main class="main content">
+        <section class="container container-features">
+            <div class="cart-feature">
+                <i class="fa-solid fa-plane-up"></i>
+                <div class="feature-content">
+                    <span>Envio gratuito a nivel mundial</span>
+                    <p>En pedido superior a $150</p>
+                </div>
+            </div>
+            <div class="cart-feature">
+                <i class="fa-solid fa-wallet"></i>
+                <div class="feature-content">
+                    <span>Contrareembolso</span>
+                    <p>100% garantia de devolución de dinero</p>
+                </div>
+            </div>
+            <div class="cart-feature">
+                <i class="fa-solid fa-gift"></i>
+                <div class="feature-content">
+                    <span>Targeta regalo especial</span>
+                    <p>Ofrece bonos especiales con regalo</p>
+                </div>
+            </div>
+            <div class="cart-feature">
+                <i class="fa-solid fa-headset"></i>
+                <div class="feature-content">
+                    <span>Soporte al Cliente 24/7</span>
+                    <p>Llamanos 24/7 al 6673023346</p>
+                </div>
+            </div>
+        </section>
+        <section class="container top-categories">
+            <h1 class="heading-1">Mejores Categorías</h1>
+            <div class="container-categories">
+                <div class="card-category category-moca">
+                    <p>Café moca</p>
+                    <span>Ver más</span>
+                </div>
+                <div class="card-category category-expreso">
+                    <p>Expreso Americano</p>
+                    <span>Ver más</span>
+                </div>
+                <div class="card-category category-capuchino">
+                    <p>Capuchino</p>
+                    <span>Ver más</span>
+                </div>
+            </div>
+        </section>
+        <section class="container top-products">
+            <h1 class="heading-1">Mejores Productos</h1>
+            <div class="container-options">
+                <span class="active">Destacados</span>
+                <span>Más recientes</span>
+                <span>Mejores vendidos</span>
+            </div>
+
+            <div class="container-products">
+                <!-- Producto 1 -->
+                <div class="card-product">
+                    <div class="container-img">
+                        <img src="images/cafe-irish.jpg" alt="cafe irish">
+                        <span class="discount">-13%</span>
+                        <div class="button-group">
+                            <span>
+                                <i class="fa-regular fa-eye"></i>
+                            </span>
+                            <span>
+                                <i class="fa-regular fa-heart"></i>
+                            </span>
+                            <span>
+                                <i class="fa-regular fa-code-compare"></i>
+                            </span>
+                        </div>
+                    </div>
+
+                    <div class="content-card-product">
+                        <div class="stars">
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-regular fa-star"></i>
+                        </div>
+                        <h3>Café Irish</h3>
+                        <span class="add-cart">
+                            <i class="fa-solid fa-basket-shopping"></i>
+                        </span>
+                        <p class="price">$4.70 <span>$5.40</span></p>
+                    </div>
+                </div>
+                <!-- Producto 2 -->
+            </div>
+        </section>
+    </main>
 </body>
 
 </html>
